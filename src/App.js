d364470ebff0bafc00010fb36fb908651caa0e78
vileeni2 <villesinkkonen12@gmail.com>
2023-10-19 18:59:02 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import { useState , useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
+const URL = "https://www.themealdb.com/api/json/v1/1/random.php"
+
 function App() {
+
+  const [meal, setMeal] = useState('')
+  const [category, setCategory] = useState('')
+  const [instructions, setInstructions] = useState('')
+  const [ingredients, setIngredients] = useState([])
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        const mealData = response.data.meals[0]
+        setMeal(mealData.strMeal)
+        setCategory(mealData.strCategory)
+        setInstructions(mealData.strInstructions)
+
+        const newIngredients = []
+        for (let i = 1; i <= 20; i++) {
+          const ingredient = mealData[`strIngredient${i}`]
+          if (ingredient) {
+            newIngredients.push(ingredient)
+          }
+        }
+        setIngredients(newIngredients)
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="container">
+      <h1>Meal of the day</h1>
+      <h2>{meal}</h2>
+      <h4>Category:</h4>
+      <p>{category}</p>
+      <h4>Ingredients:</h4>
+      <ul>
+        {ingredients.map((ingredient, index) => (
+          <li key={index}>{ingredient}</li>
+        ))}
+      </ul>
+      <h4>Instructions:</h4>
+      <p>{instructions}</p>
+      
     </div>
-  );
+  )
 }
 
 export default App;
